2979baa59d30ca11c6d299b2ccbe6717a67678e9
Yannik Roth <roth.yannik@hotmail.com>
2020-04-09 14:07:28 +0200

    [roy] - create repositories for future interaction

---
+++ app/src/main/java/ch/fhnw/shoppingorganizer/model/database/ShoppingListItemRepository.java
@@ -0,0 +1,24 @@
+package ch.fhnw.shoppingorganizer.model.database;
+
+import com.activeandroid.Model;
+import com.activeandroid.query.Select;
+
+import java.util.List;
+
+import ch.fhnw.shoppingorganizer.model.businessobject.ShoppingListItem;
+
+public class ShoppingListItemRepository extends AbstractRepository{
+
+    @Override
+    public List<ShoppingListItem> getAllItems() {
+        return new Select().from(ShoppingListItem.class).execute();
+    }
+
+    /**
+     * Returns only one shopping list item with provided id.
+     * Returns null if tupel with given id does not exist
+     */
+    public ShoppingListItem getShoppingListById(long id){
+        return (ShoppingListItem) super.getById(ShoppingListItem.class, id);
+    }
+}
--- app/src/main/java/ch/fhnw/shoppingorganizer/view/MainActivity.java
+++ app/src/main/java/ch/fhnw/shoppingorganizer/view/MainActivity.java
@@ -1,18 +1,96 @@
 package ch.fhnw.shoppingorganizer.view;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 
 import com.activeandroid.ActiveAndroid;
+import com.activeandroid.query.Select;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 import ch.fhnw.shoppingorganizer.R;
+import ch.fhnw.shoppingorganizer.model.businessobject.Category;
+import ch.fhnw.shoppingorganizer.model.businessobject.ShoppingItem;
+import ch.fhnw.shoppingorganizer.model.businessobject.ShoppingItemBuilder;
+import ch.fhnw.shoppingorganizer.model.businessobject.ShoppingList;
+import ch.fhnw.shoppingorganizer.model.businessobject.ShoppingListBuilder;
+import ch.fhnw.shoppingorganizer.model.businessobject.ShoppingListItem;
+import ch.fhnw.shoppingorganizer.model.businessobject.ShoppingListItemBuilder;
+import ch.fhnw.shoppingorganizer.model.database.ShoppingListRepository;
 
 public class MainActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        runDatabaseQueries();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void runDatabaseQueries(){
+//        //create the shopping list
+        ShoppingList shoppingList = new ShoppingListBuilder()
+                .withListName("Kleiderliste")
+                .build();
+//        shoppingList.save();
+//
+//        ShoppingItem shoppingItem = new ShoppingItemBuilder()
+//                .withItemActive(true)
+//                .withCategory(Category.VEGETABLES)
+//                .withPrice(BigDecimal.valueOf(10))
+//                .withItemName("Banana")
+//                .build();
+//        shoppingItem.save();
+//
+//        ShoppingItem shoppingItem2 = new ShoppingItemBuilder()
+//                .withItemActive(true)
+//                .withCategory(Category.MEAT)
+//                .withPrice(BigDecimal.valueOf(20))
+//                .withItemName("Steak")
+//                .build();
+//        shoppingItem2.save();
+//
+//        ShoppingListItem shoppingListItem = new ShoppingListItemBuilder()
+//                .withQuantity(1)
+//                .withItemState(true)
+//                .withShoppingItem(shoppingItem)
+//                .withShoppingList(shoppingList)
+//                .build();
+//        shoppingListItem.save();
+//
+//        ShoppingListItem shoppingListItem2 = new ShoppingListItemBuilder()
+//                .withQuantity(2)
+//                .withItemState(true)
+//                .withShoppingItem(shoppingItem2)
+//                .withShoppingList(shoppingList)
+//                .build();
+//        shoppingListItem2.save();
+
+        new Select().from(ShoppingList.class).join(ShoppingListItem.class).on("ShoppingList.Id = ShoppingListItem.Id").execute();
+
+        List<ShoppingListItem> myList =
+                new Select().from(ShoppingListItem.class).join(ShoppingList.class).on("ShoppingListItem.shoppingList = ShoppingList.Id").execute();
+
+        List<ShoppingListItem> myList2 =
+                new Select().from(ShoppingListItem.class).where("shoppingList =?", new String[]{"1"}).execute();
+
+        BigDecimal v = myList.stream().map(e -> e.getTotalItemPrice()).reduce(BigDecimal.ZERO, BigDecimal::add);
+
+        ShoppingList sl = new Select().from(ShoppingList.class).where("Id=1").executeSingle();
+
+        ShoppingListRepository rep = new ShoppingListRepository();
+        List<ShoppingListItem> sss = rep.getShoppingListItems(sl);
+        BigDecimal vv = sss.stream().map(e -> e.getTotalItemPrice()).reduce(BigDecimal.ZERO, BigDecimal::add);
+
+        ShoppingListRepository repList = new ShoppingListRepository();
+        ShoppingList l = repList.getShoppingListById(3);
+        List<ShoppingList> abc = repList.getAllItems();
     }
 }
