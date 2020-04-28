784da474ce33e2a3f41db0212c2480f99e74d66c
Yannik Roth <roth.yannik@hotmail.com>
2020-04-28 15:50:19 +0200

    [roy] - enable app to open from email attachmanent

---
+++ app/src/main/java/ch/fhnw/shoppingorganizer/view/ImportActivity.java
@@ -0,0 +1,16 @@
+package ch.fhnw.shoppingorganizer.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ch.fhnw.shoppingorganizer.R;
+
+public class ImportActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_import);
+    }
+}
